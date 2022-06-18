14c9d4eb88473ebe6942004b27bad1b9d24519b4
yarosh <yarosh.nv@yandex.ru>
2022-06-18 17:33:26 +0300

    .

---
+++ src/HorstmanTasks/First/FirstTasks1.scala
@@ -0,0 +1,20 @@
+package HorstmanTasks.First
+import scala.math._
+
+object FirstTasks1 extends App{
+  // Задание 2
+  var number1, number2: Int = 3
+
+  println(number1)
+  println(pow(sqrt(number1),2))
+
+  // Задание 4
+
+  println("crazy" * 3)
+
+  // Задание 6
+
+  println(BigInt(2) pow 1024)
+
+
+}
---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,52 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  // Val`s нельзя изменять (аля константа)
+  val x: Int = 42;
+  println(x)
+
+  // Компилятор может узнать какой тип данных используется в зависимости от значения (можно не указывать тип)
+  val y = "45"
+  println(y)
+
+  val aString: String = "abc"
+  val aString2 = "test"
+
+  val aBoolean: Boolean = true
+  val aBoolean2 = false
+
+  val aChar: Char = 'a'
+  val aChar2 = 'b'
+
+  val aInt: Int = 999999999
+  val aInt2 = 45
+
+  val aShort: Short = 9999
+  val aShort2 = 9999
+
+  val aLong: Long = 999999999999999999L
+  val aLong2 = 999999999999999999L
+
+  val aFloat: Float = 2.0f
+  val aFloat2 = 2.0f
+
+  val aDouble: Double = 222.0
+  val aDouble2 = 222.0
+
+  // Изменяемые переменные обозначаются словом "var"
+  var aVariable: Int = 55
+  aVariable = 65
+
+  println(aVariable)
+
+  // Предпочтительней использовать val, так что если есть возможность
+  // не использовать var, то лучше использовать val
+
+  val first, second :String = "Test"
+  println(first + second)
+
+  var test = "Test".intersect("test")
+
+  println(test)
+
+}
