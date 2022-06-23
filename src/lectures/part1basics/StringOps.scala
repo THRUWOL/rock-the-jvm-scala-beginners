e9f784e16849251df9355c122899322ad4c0f4e8
thruwol <yarosh.nv@yandex.ru>
2022-06-23 11:24:20 +0300

    rock scala

---
+++ src/lectures/part1basics/StringOps.scala
@@ -0,0 +1,15 @@
+package lectures.part1basics
+
+object StringOps extends App {
+
+  val str: String = "Hello, I am learning Scala"
+
+  println(str.charAt(2))
+  println(str.substring(7, 11))
+  println(str.split(" ").toList)
+  println(str.startsWith("Hello"))
+  println(str.replace(" ", "-"))
+  println(str.toLowerCase())
+  println(str.toUpperCase())
+  println(str.length)
+}
